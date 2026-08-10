6c04e8af544db4208c9f8bbfe918e918b96bee5c
Bryan Tiago <bryanbragaa.bj@gmail.com>
2026-08-10 15:07:43 -0300

    Adiciona fotos autorizadas na prova social

--- src/components/InterestProof/InterestProof.jsx
+++ src/components/InterestProof/InterestProof.jsx
@@ -1,8 +1,14 @@
 import { useSyncExternalStore } from 'react'
 import { getInterestCount, getServerInterestCount, subscribeInterestCount } from '../../data/siteData.js'
+import avatar1 from '../../assets/interest-avatars/avatar-1.png'
+import avatar2 from '../../assets/interest-avatars/avatar-2.png'
+import avatar3 from '../../assets/interest-avatars/avatar-3.png'
+import avatar4 from '../../assets/interest-avatars/avatar-4.png'
 import './InterestProof.css'
 
-export default function InterestProof({ photos = [], className = '' }) {
+const authorizedInterestPhotos = [avatar1, avatar2, avatar3, avatar4].map((src) => ({ src }))
+
+export default function InterestProof({ photos = authorizedInterestPhotos, className = '' }) {
   const interestCount = useSyncExternalStore(subscribeInterestCount, getInterestCount, getServerInterestCount)
   const authorizedPhotos = photos.slice(0, 4)
 
